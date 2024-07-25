fafac437552bb4c4318fa684c1519c98208cb333
MichaelWood <mgwood22@sky.com>
2024-07-25 14:41:46 +0100

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Subscribe from './Subscribe';  
+import SpecialOffer from './SpecialOffer';  
+
+export { 
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  CustomerReviews,
+  Footer,
+  Subscribe,
+  SpecialOffer
+}
